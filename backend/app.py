fd5e60ba45f021dd3f11b3271ba961e2a28da998
DaHyun <tanyifan2001@sina.com>
2022-08-01 13:02:49 +0800

    TODO:login

---
+++ backend/app.py
@@ -0,0 +1,33 @@
+import database
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    '''
+    登录接口
+    TODO：之后可能会加一些用户权限系统
+     
+    Parameters:
+     user : str - 用户名
+     password : str - 密码
+     
+    Returns:
+     status : bool - 一个布尔值表示是否登陆成功
+     
+    Raises:
+     本函数不应该报错
+    '''
+    user = request.form['user']
+    password = request.form['password']
+    
+    status = database.identify(user, password)
+        
+    return jsonify({'status':status})
---
+++ backend/database.py
@@ -0,0 +1,18 @@
+
+def identify(user : str, password : str):
+    '''
+    TODO
+    身份识别函数，识别用户是否存在/用户名与密码是否匹配
+     
+    Parameters:
+     uese - 用户名
+     password - 密码
+     
+    Returns:
+     一个布尔值表示用户名是否匹配
+     TODO：之后可能会增加一些用户的权限系统用于区分管理员和普通用户
+     
+    Raises:
+     本函数不应该报错
+    '''
+    return True
