2d6fa8b9d70f7cfd822261bd8c5b6855b8b01f21
llz <shaomi_lin@126.com>
2022-08-15 14:47:33 +0800

    fix package-lock.json conflict

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -9,7 +9,12 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     CodeEditor: typeof import('./src/components/CodeEditor.vue')['default']
     CodeViewer: typeof import('./src/components/CodeViewer.vue')['default']
+    Model_info_manage_addserver: typeof import('./src/components/model_info_manage_addserver.vue')['default']
+    Model_info_manage_deploy: typeof import('./src/components/model_info_manage_deploy.vue')['default']
+    Model_info_manage_overview: typeof import('./src/components/model_info_manage_overview.vue')['default']
+    Model_info_manage_test: typeof import('./src/components/model_info_manage_test.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    User_info: typeof import('./src/components/user_info.vue')['default']
   }
 }
