e372e2828933b30b72e4e337ba3cf583831482ab
shuibuxingha <15510682898@163.com>
2022-08-16 15:07:26 +0800

    增加模型有效性验证

--- backend/getInfoFromModel.py
+++ backend/getInfoFromModel.py
@@ -141,5 +141,183 @@ def getmodelinfo(modelpath : str):
             "predict_variate":predict_variate_list}
 
 
+def checkmodel(user : str, password : str, modeltype:str, modelname : str):
+    '''
+    Args:
+        user: 用户名
+        password: 密码
+        modeltype: 用户通过表单选择， pmml或onnx
+        modelname: 输入的模型名称,模型为已经储存在/backend/model里的文件，需要带.pmml或.onnx后缀
 
+    Returns:
+        legal:bool值，True表示模型合法，False表示模型不合法。若模型不合法，getmodelinfo函数返回值可能为空
+        error_info:str 在不合法时返回报错，合法时为空字符串
+    '''
+    import sys
+    import onnx
+    import re
+    from xml.etree import ElementTree as ET
+    import check_pmml_xsd.pmml_441
+    import check_pmml_xsd.pmml_44
+    import check_pmml_xsd.pmml_43
+    import check_pmml_xsd.pmml_42
+    import check_pmml_xsd.pmml_41
+    import check_pmml_xsd.pmml_40
+    import check_pmml_xsd.pmml_32
+    valid=False
+    error_info='model is invalid'
+    model = re.search(r'....$', modelname)
+    file_path = "./model/" + modelname
+    if (modeltype == "pmml") or (modeltype == "PMML"):
+        # 验证是否为pmml模型
+        if model.group() != "pmml":
+            valid = False
+            error_info = "model is invalid: not a pmml model"
+            return valid, error_info
+        #检验是否是合格的xml
+        try:
+            ET.parse(file_path)
+            #print("xml ok")
+        except Exception as e:
+            error_info =e
+            valid=False
+            return valid,error_info
+        #是否符合对应版本的xsd
+        #读取版本
+        try:
+            with open(file_path, encoding='utf-8') as file_obj:
+                contents = file_obj.read()
+            version = re.search(r'<PMML.*?version="(.*?)"', contents)
+            version=version.group(1)
+            #print("version",version)
+        except Exception:
+            error_info = "model is invalid:can't get version"
+            valid = False
+            return valid, error_info
+        #验证对应版本
+        if version=="4.4.1":
+            try:
+                model = check_pmml_xsd.pmml_441.parse(file_path, silence=True)
+            except Exception as e:
+                error_info = "model is invalid:"+e
+                valid = False
+            else:
+                error_info = ""
+                valid = True
+        elif version=="4.4":
+            try:
+                model = check_pmml_xsd.pmml_44.parse(file_path, silence=True)
+            except Exception as e:
+                error_info = "model is invalid:"+e
+                valid = False
+            else:
+                error_info = ""
+                valid = True
+        elif version=="4.3":
+            try:
+                model = check_pmml_xsd.pmml_43.parse(file_path, silence=True)
+            except Exception as e:
+                error_info = "model is invalid:"+e
+                valid = False
+            else:
+                error_info = ""
+                valid = True
+        elif version=="4.2":
+            try:
+                model = check_pmml_xsd.pmml_42.parse(file_path, silence=True)
+            except Exception as e:
+                error_info = "model is invalid:"+e
+                valid = False
+            else:
+                error_info = ""
+                valid = True
+        elif version=="4.1":
+            try:
+                model = check_pmml_xsd.pmml_41.parse(file_path, silence=True)
+            except Exception as e:
+                error_info = "model is invalid:"+e
+                valid = False
+            else:
+                error_info = ""
+                valid = True
+        elif version=="4.0":
+            try:
+                model = check_pmml_xsd.pmml_40.parse(file_path, silence=True)
+            except Exception as e:
+                error_info = "model is invalid:"+e
+                valid = False
+            else:
+                error_info = ""
+                valid = True
+                #print("haha")
+        elif version=="3.2":
+            try:
+                model = check_pmml_xsd.pmml_32.parse(file_path, silence=True)
+            except Exception as e:
+                error_info = "model is invalid:"+e
+                valid = False
+            else:
+                error_info = ""
+                valid = True
+        else:
+            # 若version为4.2.1之类无法转成float的且不在枚举范围内的，4.4可验证4.1-4.4，3.2可验证3.0-3.2
+            version=version[0]
+            #print("haha")
+            if version=='4':
+                try:
+                    model = check_pmml_xsd.pmml_44.parse(file_path, silence=True)
+                except Exception as e:
+                    error_info = "model is invalid:" + e
+                    valid = False
+                else:
+                    error_info = ""
+                    valid = True
+            elif version=='3':
+                try:
+                    model = check_pmml_xsd.pmml_32.parse(file_path, silence=True)
+                except Exception as e:
+                    error_info = "model is invalid:" + e
+                    valid = False
+                else:
+                    error_info = ""
+                    valid = True
+            else:
+                #在2.1以前的版本
+                error_info = ""
+                valid = True
+    elif (modeltype=="onnx") or (modeltype=="ONNX"):
+        #是否为onnx
+        if model.group() !="onnx":
+            valid=False
+            error_info="model is invalid: not an onnx model"
+            return valid,error_info
+        #加载
+        try:
+            model = onnx.load(file_path)
+        except:
+            error_info=sys.exc_info()
+            valid=False
+            #print(error_info)
+        else:
+            #检验有效性
+            try:
+                onnx.checker.check_model(model)
+            except onnx.checker.ValidationError as e:
+                error_info = error_info+e
+                #print(error_info)
+                valid=False
+            else:
+                valid = True
+                error_info=''
+                #print("haha")
+    else:#传入的modeltype有问题 仅能接受pmml、onnx和PMML ONNX 查看拼写是否正确
+        valid=False
+        error_info="model type error(only support pmml or onnx)"
+
+    return valid,error_info
+
+'''if __name__ == '__main__':
+    a,b=checkmodel("haha","haha","onnx","t.pmml")
+    print(a)
+    print(b)'''
 
