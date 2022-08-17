ccc79d0eeda40bc43d423b1b32e675d412e6f27d
shuibuxingha <15510682898@163.com>
2022-08-17 13:46:35 +0800

    更新upload接口

--- backend/app.py
+++ backend/app.py
@@ -56,26 +56,57 @@ def login():
 
     return jsonify({'status':status})
 
-@app.route('/upload',methods=["POST"])
+@app.route('/upload',methods=["GET","POST"])
 def upload():
+    '''
+    功能：接收上传的模型，验证有效性，若有效，读取模型信息并存储模型，若无效，返回错误信息
+
+    需要前端传入的参数：
+    user：str 用户名
+    password：str 密码
+    modeltype：str 模型类别 pmml或onnx 全大写或全小写
+    file：模型文件 pmml或onnx
+    time：str 创建时间
+    description： str 模型描述
+
+    Returns:
+        'status':bool 表示上传模型成功或失败
+        'error':str 若status为False，返回错误信息，若status为True为空字符串
+    '''
     import os
-    #待debug。。route也未和前端统一
+    import getInfoFromModel
+    #route未和前端统一
     file = request.files.get('file')
     if file is None:  #接受失败
         return {
-            'message': "文件上传失败"
+            'status': False,
+            'error':"文件上传失败"
         }
     file_name = file.filename.replace(" ", "")
     #print("获取上传文件的名称为[%s]\n" % file_name)
     #保存文件
+    file_path=os.path.dirname(__file__) + '/model/' + file_name
     file.save(os.path.dirname(__file__) + '/model/' + file_name)
-    #记录模型对应用户
+    # 获取前端传入信息
     user = request.form['user']
     password = request.form['password']
+    modeltype = request.form['modeltype']
+    time = request.form['time']
+    description = request.form['description']
+    #检测模型有效性
+    valid,err_info=getInfoFromModel.checkmodel("user",'password',modeltype,file_name)#先验证有效性再保存，这一步目前不验证用户密码
+    if valid:#模型有效
+        #从模型中读取信息
+        dict=getInfoFromModel.getmodelinfo(file_name)
+        #储存模型
+        database.savemodel(user, password, file_name,modeltype,time,file_path,description,
+                           dict['engine'],dict['algorithm'],dict['input_variate'],dict['predict_variate'])
+    else:
+        pass
 
-    database.savemodel(user, password, file_name)
+    return jsonify({'status':valid,
+                    'error':err_info})
 
-    return 'success'
 
 @app.route('/fake_getmodelinfo',methods=["GET"])
 def fake_getmodelinfo():
