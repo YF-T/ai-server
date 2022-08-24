6faba04f91507bdd293e3ab27f369ea033302447
llz <shaomi_lin@126.com>
2022-08-24 10:26:47 +0800

    任务详情页面

--- backend/user_prepare.py
+++ backend/user_prepare.py
@@ -1,23 +1,4 @@
-import json 
-import os 
-import prepare as default_prepare 
-def prepare(input_type, file): 
-  if isinstance(file, str): 
-    # 如果您传入的是字符串可以直接使用json.loads 
-    return json.loads(file) 
-  else: 
-    filepath = './input_file/' + file.filename 
-    file.save(filepath) 
-    data = None 
-    # todo # 如果您传入的是jpg 
-    # data = default_prepare.readimg(input_type[0], filepath, file) 
-    # 如果您传入的是mp4 
-    # data = default_prepare.readmp4(input_type[0], filepath, file) 
-    # 如果您传入的是txt 
-    # data = default_prepare.readtxt(input_type, filepath, file)  
-    # 如果您传入的是csv 
-    # data = default_prepare.readcsv(input_type, filepath, file) 
-    # 如果您传入的是zip 
-    # data = default_prepare.readzip(input_type, filepath, file) 
-    os.remove(filepath) 
-    return data
+import json 
+def prepare(model_input_type,file):
+    print(file)
+    return json.loads(file)
--- frontend/src/views/TaskDetail.vue
+++ frontend/src/views/TaskDetail.vue
@@ -3,39 +3,51 @@
     <h2>任务详情页面</h2>
     <p>当前模型：{{ modelName }}</p>
     <el-card class="card">
-        <p>任务 id: {{ taskId }}</p>
-        <p>任务状态: {{ taskStatus }}</p>
-        <p>任务结果：</p>
-        <CodeViewer :code="result" language="javascript"/>
+      <p>任务 id: {{ taskId }}</p>
+      <p>任务状态: {{ taskStatus }}</p>
+      <p>任务结果：</p>
+      <CodeViewer :code="result" language="javascript" />
     </el-card>
   </div>
 </template>
 <script setup lang="ts">
 import { ElCard } from 'element-plus'
-import CodeViewer from '@/components/CodeViewer.vue';
-import { ref } from 'vue'
+import CodeViewer from '@/components/CodeViewer.vue'
+import { ref, onMounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
-import { useStore } from 'vuex';
-
+import { useStore } from 'vuex'
+import { request } from '../Util'
 const route = useRoute()
 const store = useStore()
 
-const task = store.state.tasks.find((task: any) => task.taskid == route.params.id)
+const task = store.state.tasks.find(
+  (task: any) => task.taskid == route.params.id
+)
 const modelName = ref(store.state.modelName)
-const taskId= ref(route.params.id)
+const deployName = ref(store.state.webname)
+const taskId = ref(route.params.id)
 const taskStatus = ref(task.status)
-const result = ref('')
 
+onMounted(() => {
+  const path = `http://127.0.0.1:5000/get_result_delayresponse/${deployName}/${taskId}`
+  const param = new FormData()
+  request(path, param).then((res: any) => {
+    if (res.data.status == 'success') {
+      result.value = res.data.output
+    }
+  })
+})
+const result = ref('')
 </script>
 <style scoped>
 .task_detail {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
 .card {
-    width: 50%;
+  width: 50%;
 }
 </style>
