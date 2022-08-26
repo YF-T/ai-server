bb1dd822afcb6a66e54a6f95db2649af13ffcd39
DaHyun <tanyifan2001@sina.com>
2022-08-26 22:10:04 +0800

    文档

--- backend/user_prepare.py
+++ backend/user_prepare.py
@@ -1,28 +1,26 @@
-import zipfile
-import pandas as pd
 import json
 import os
-
-def process_text_to_json(fileaddress: str):
-    f = open(fileaddress)
-    string = f.read()
-    f.close()
-    json_dict = json.loads(string)
-    return json_dict
-def process_base64_to_csv(file, id: int):
-    path = './output/zip/' + str(id)
-    #os.makedirs(path)
-    f = zipfile.ZipFile(file)
-    for fz in f.namelist():  
-        f.extract(fz, path)
-    file_list = os.listdir(path)
-    df = pd.DataFrame()
-    for txt_address in file_list:
-        temp_address = path + '/' + txt_address
-        temp_json_dict = process_text_to_json(temp_address)
-        temp_df = pd.DataFrame(temp_json_dict, index=[0])
-        df = pd.concat([df, temp_df])
-    return df
-    
-def prepare(model_input_type,file):
-    return process_base64_to_csv(file, 100)
+import prepare as preprocess
+def prepare(input_type, file):
+    if isinstance(file, str):
+        # 如果您传入的是字符串可以直接使用json.loads
+        return json.loads(file)
+    else:
+        filepath = './input_file/' + file.filename
+        file.save(filepath)
+        data = None
+        
+        # todo
+        # 如果您传入的是jpg
+        # data = preprocess.readimg(input_type[0], filepath, file)
+        # 如果您传入的是mp4
+        # data = preprocess.readmp4(input_type[0], filepath, file)
+        # 如果您传入的是txt
+        # data = preprocess.readtxt(input_type, filepath, file)
+        # 如果您传入的是csv
+        # data = preprocess.readcsv(input_type, filepath, file)
+        # 如果您传入的是zip
+        data = preprocess.readzip(input_type, filepath, file)
+        
+        os.remove(filepath)
+        return data
